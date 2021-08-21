169240d75df36ce4f98fd0b7b15009e74ae03b28
tonocp <amcp88@gmail.com>
2021-08-21 17:10:24 +0200

    Angular GIF's App

---
+++ src/app/gifs/gifs-page/gifs-page.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs-page',
+  templateUrl: './gifs-page.component.html',
+  styles: [
+  ]
+})
+export class GifsPageComponent {
+
+  constructor() { 
+
+    
+    
+  }
+
+  
+
+}
